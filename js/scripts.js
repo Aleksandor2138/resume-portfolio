9f88fba6b7c83b9e6aa559bd96444eae0cc8a99c
Aleksandor2138 <99901672+Aleksandor2138@users.noreply.github.com>
2022-07-28 17:39:00 +0300

    top + grid.main

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+// mob-menu
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector(".mobile-open-btn"),
+    closeMenuBtn: document.querySelector(".mobile-close"),
+    menu: document.querySelector(".mobile-menu"),
+    // body: document.querySelector("body"),
+  };
+
+  refs.openMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle("is-hidden");
+    // refs.body.classList.toggle("no-scroll");
+  }
+})();
+
+// button top   When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById('myBtn').style.display = 'block';
+  } else {
+    document.getElementById('myBtn').style.display = 'none';
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
